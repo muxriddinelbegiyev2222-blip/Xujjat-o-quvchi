17d679784adea02d55ac1123a8f7ee364aa4cf47
muxriddinelbegiyev2222-blip <muxriddinelbegiyev2222@gmail.com>
2026-09-17 11:08:27 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,42 @@
+# config.py
+
+REGIONS = [
+    "Toshkent_shahri",
+    "Qoraqalpogiston_Respublikasi",
+    "Andijon_viloyati",
+    "Buxoro_viloyati",
+    "Jizzax_viloyati",
+    "Qashqadaryo_viloyati",
+    "Navoiy_viloyati",
+    "Namangan_viloyati",
+    "Samarqand_viloyati",
+    "Surxondaryo_viloyati",
+    "Sirdaryo_viloyati",
+    "Toshkent_viloyati",
+    "Fargona_viloyati",
+    "Xorazm_viloyati"
+]
+
+ORGANIZATIONS = [
+    "Kadastr_Agentligi",
+    "Davlat_Kadastrlari_Palatasi"
+]
+
+LAW_ENFORCEMENT = [
+    "Bosh_Prokuratura",
+    "IIV",
+    "DXX",
+    "Sudlar",
+    "Boshqa_Organlar"
+]
+
+DOC_TYPES = [
+    "Buyruqlar",
+    "Topshiriqlar",
+    "Xatlar",
+    "Taqdimnomalar",
+    "Arizalar",
+    "Boshqa"
+]
+
+BASE_DIR = "Arxiv_Baza"
